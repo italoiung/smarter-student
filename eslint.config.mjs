f33f823eababef3dff3e50179873cbeb90884b26
Ítalo Iung <italoiung@gmail.com>
2024-11-06 11:26:50 -0300

    chore: add eslint + prettier config

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  eslintPluginPrettier,
+);
